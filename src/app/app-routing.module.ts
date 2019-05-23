3ca2a7fd2bf9c47fe2932b8f780aecb38fda8d11
fabischolasi <fabischapeli97@gmail.com>
2019-05-23 19:11:20 +0300

    add routing in app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { ArtistsComponent } from './artists/artists.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomepageComponent },
+  { path: 'artists', component: ArtistsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
