8b9ce13f0859235514d5801352881dd0473c85bc
fabischolasi <fabischapeli97@gmail.com>
2019-05-29 12:38:25 +0300

    stream video from api via frontend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { ArtistsComponent } from './artists/artists.component';
 import { ArtistComponent } from './artists/artist/artist.component';
+import { PlaylistsComponent } from './playlists/playlists.component';
+import { VideoComponent } from './video/video.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomepageComponent },
   { path: 'artists', component: ArtistsComponent },
   { path: 'artists/:id', component: ArtistComponent },
+  { path: 'albums/:id', component: PlaylistsComponent },
+  { path: 'v/:name', component: VideoComponent },
 ];
 
 @NgModule({
